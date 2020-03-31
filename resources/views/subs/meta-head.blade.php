592559d69c98b9250f4d2d26397f40c27a8dabb0
Pandalisme <agapearimatea99@gmail.com>
2020-03-31 16:33:29 +0700

    initial init

---
+++ resources/views/subs/meta-head.blade.php
@@ -0,0 +1,20 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TOKlinik</title>
+
+  <!-- Favicons -->
+  <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{asset('assets/lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <!--external css-->
+  <link href="{{asset('assets/lib/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/zabuto_calendar.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/gritter/css/jquery.gritter.css') }}" />
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/css/style-responsive.css') }}" rel="stylesheet">
+  <script src="{{ asset('assets/lib/chart-master/Chart.js') }}"></script>
+</head>
