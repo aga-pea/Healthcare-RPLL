31ea18d88b0685fa37a09720a9c5b63c37c9324f
aga-pea <agapearimatea99@gmail.com>
2020-04-16 22:24:08 +0700

    Update mail_compose.blade.php

--- resources/views/Patient/mail_compose.blade.php
+++ resources/views/Patient/mail_compose.blade.php
@@ -27,7 +27,7 @@
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
       </div>
       <!--logo start-->
-      <a href="{{url('/patient_main')}}"" class=" logo"><b>TO<span>KLINIK</span></b></a>
+      <a href="{{url('/patient_main')}}"" class=" logo"><b>TOK<span>LINIK</span></b></a>
       <!--logo end-->
       <div class="nav notify-row" id="top_menu">
         <!--  notification start -->
