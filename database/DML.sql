01258a84ac760b55a9e4a9d6810c23da227eb478
Dionisius Pratama <dionisiuspr@gmail.com>
2020-04-17 22:37:17 +0700

    add DDL and DML
    DML in progress editing

---
+++ database/DML.sql
@@ -0,0 +1,7 @@
+insert into 
+	patient(patient_id, patient_name, patient_address, patient_age, patient_dob, patient_gender, patient_uname, patient_pwd) 
+	values('A1', 'Fikri', 'Taman Rahayu', 20, '1999-04-20', 'L', 'fikri01', 'fikri123');
+
+
+
+insert into medical_staff(medstaff_id, medstaff_age, medstaff_name, medstaff_uname, medstaff_pwd, department_id) values(1, 10, 'd', 'ion', 'ion123', 1) ;
