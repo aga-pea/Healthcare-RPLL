5f44228f8e48b723f65268f8c7290e5a5447e7dd
aga-pea <agapearimatea99@gmail.com>
2020-05-02 22:37:25 +0700

    Update DML.sql

--- database/DML.sql
+++ database/DML.sql
@@ -7,12 +7,12 @@ insert INTO
 	values(2, 'Departemen Penyakit Infeksius');
 
 insert into 
-	patient(patient_id, patient_name, patient_address, patient_age, patient_dob, patient_gender, patient_uname, patient_pwd) 
-	values('A1', 'Fikri', 'Taman Rahayu', 20, '1999-04-20', 'L', 'fikri01', 'fikri123');
+	patient(patient_name, patient_address, patient_age, patient_dob, patient_gender, patient_uname, patient_pwd) 
+	values('Fikri', 'Taman Rahayu', 20, '1999-04-20', 'L', 'fikri01', 'fikri123');
 
 insert into 
-	patient(patient_id, patient_name, patient_address, patient_age, patient_dob, patient_gender, patient_uname, patient_pwd) 
-	values('A2', 'Gwen', 'Taman Kopo', 18, '2001-06-10', 'P', 'gwen10', 'gwen123');
+	patient(patient_name, patient_address, patient_age, patient_dob, patient_gender, patient_uname, patient_pwd) 
+	values('Gwen', 'Taman Kopo', 18, '2001-06-10', 'P', 'gwen10', 'gwen123');
 
 insert into 
 	medical_staff(medstaff_id, medstaff_age, medstaff_name, medstaff_uname, medstaff_pwd, department_id) 
