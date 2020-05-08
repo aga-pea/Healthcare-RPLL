2962d6b8d2dcd0ca7cfb2fbcb7c815ded65cfe40
matthewsutanto <41150197+matthewsutanto@users.noreply.github.com>
2020-05-08 21:38:01 +0700

    update invoice

--- app/Http/Controllers/AddInvoiceController.php
+++ app/Http/Controllers/AddInvoiceController.php
@@ -12,24 +12,23 @@ class AddInvoiceController extends Controller
         $invoice = new InvoiceUseCase();
         $invoiceData = $invoice->searchInvoiceByStatus("Active");
         $list_patient= [];
-        $patient_name=[];
+        $patient_id=[];
         foreach($invoiceData as $data)
         {
             $array=[];
             $patient = new PatientUseCase();
             $patientData = $patient->getPatientById($data->patient_id);
-            if (in_array($patientData->patient_name, $patient_name, true) != true)
+            if (in_array($patientData->patient_id, $patient_id, true) != true)
             {
-                print("halo");
                 $array["patient_id"]=$patientData->patient_id;
                 $array["patient_name"]=$patientData->patient_name;
                 array_push($list_patient,$array);
-                array_push($patient_name, $array["patient_name"]);
+                array_push($patient_id, $array["patient_id"]);
             }
             
         }
-        print(var_dump($patient_name)."<br>");
+        print(var_dump($patient_id)."<br>");
         print(var_dump($list_patient));
-        // return view('Cashier/invoice');
+        // return view('Cashier/invoice_main');
     }
 }
