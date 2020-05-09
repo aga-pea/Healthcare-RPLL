431f611561da249b3e3a681a298853536679f189
irvandigian <44873309+irvandigian@users.noreply.github.com>
2020-05-09 17:44:01 +0700

    UpdateMedicalData

--- app/Dao/MedicalDataDao.php
+++ app/Dao/MedicalDataDao.php
@@ -1,30 +1,50 @@
 <?php
-    namespace App\Dao;
-    use App\Medical_Record;
 
-    class MedicalDataDao
+namespace App\Dao;
+
+use App\Medical_Record;
+
+class MedicalDataDao
+{
+    public static function getMedicalRecord($record_patient)
     {
-        public static function getMedicalRecord($record_patient)
-        {
-            $record = Medical_Record::where('patient_id', $record_patient)->get();
-            return $record;
-        }
-
-        public static function createMedicalRecord($id_patient, $id_disease, $id_hospital, $id_visit, $anamnesia){
-            Medical_Record::create([
-            
-                'patient_id' => $id_patient,
-                'disease_id' => $id_disease,
-                'hospital_id' => $id_hospital,
-                'visit_id' => $id_visit,
-                'anamnesia' => $anamnesia
-            ]); 
-        }
-
-        public static function getMedicalDataById($record_id)
-        {
-            $record = Medical_Record::where('record_id',$record_id)->get();
-            return $record;
-        }
+        $record = Medical_Record::where('patient_id', $record_patient)->get();
+        return $record;
     }
-?>
+
+    public static function createMedicalRecord($id_patient, $id_disease, $id_hospital, $id_visit, $anamnesia)
+    {
+        Medical_Record::create([
+
+            'patient_id' => $id_patient,
+            'disease_id' => $id_disease,
+            'hospital_id' => $id_hospital,
+            'visit_id' => $id_visit,
+            'anamnesia' => $anamnesia
+        ]);
+    }
+
+    public static function getMedicalDataById($record_id)
+    {
+        $record = Medical_Record::where('record_id', $record_id)->get();
+        return $record;
+    }
+
+    public static function getMedicalDataByPatient($patient_id)
+    {
+        $record = Medical_Record::where('patient_id', $patient_id);
+        return $record;
+
+    }
+
+    public static function updateMedicalDataById($record_id, $id_disease, $id_hospital, $id_visit, $anamnesia)
+    {
+        $record = Medical_record::where('record_id', $record_id)->update(
+            ['disease_id' => $id_disease],
+            ['hospital_id' => $id_hospital],
+            ['visit_id' => $id_visit],
+            ['anamnesia' => $anamnesia]
+        );
+
+    }
+}
--- app/Use_Case/MedicalDataUseCase.php
+++ app/Use_Case/MedicalDataUseCase.php
@@ -20,9 +20,23 @@
 
         public static function getMedicalDataById($record_id)
         {
-            $cek_record =  new MedicalDataDao();
-            $med_record=$cek_record->getMedicalDataById($record_id);
+            $cek_record = new MedicalDataDao();
+            $med_record = $cek_record->getMedicalDataById($record_id);
             return $med_record;
+        }
+
+        public static function getMedicalDataByPatient($patient_id)
+        {
+            $cek_record = new MedicalDataDao();
+            $med_record = $cek_record->getMedicalDataByPatient($patient_id);
+            return $med_record;
+        }
+
+        public static function updateMedicalDataById($record_id,$id_disease, $id_hospital, $id_visit, $anamnesia)
+        {
+            $med_record = new MedicalDataDao();
+            $med_record->updateMedicalDataById($record_id,$id_disease, $id_hospital, $id_visit, $anamnesia);
+
         }
 
     }
